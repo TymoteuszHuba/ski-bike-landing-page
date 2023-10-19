7adb766404aca91beb76b6f22ca35f3e2543b98e
Tymoteusz Huba <tymoteuszhuba@MacBook-Air-Tymoteusz.local>
2023-10-19 21:49:15 +0200

    updated functionality of select from bike section

--- js/bikesControl.js
+++ js/bikesControl.js
@@ -1,7 +1,35 @@
-const sectionBikes = document.querySelector('.bikes');
 const showBtn = document.querySelector('.show-btn');
-const showBtnIcon = document.querySelector('.show-btn__icon');
+const sectionBikes = document.querySelector('.bikes');
+
+const selectBikes = document.querySelector('.bikes__select');
+const bikeCards = document.querySelector('.bikes__cards');
+const cards = document.querySelectorAll('.bikes__card');
 
 showBtn.addEventListener('click', () => {
-    sectionBikes.classList.toggle('bikes__show');
+	sectionBikes.classList.toggle('bikes__show');
 });
+
+// function checking the which card was choose
+const cardSelect = (selectedT) => {
+	cards.forEach((card) => {
+		if (selectedT === 'allBikes' || card.id === selectedT) {
+			card.style.display = 'flex';
+		} else {
+			card.style.display = 'none';
+		}
+	});
+};
+
+selectBikes.addEventListener('change', () => {
+	const selectedType = selectBikes.value;
+	// console.log(selectedType);
+
+	cardSelect(selectedType);
+});
+
+cards.forEach((card) => {
+	card.addEventListener('mouseover', (event) => {
+		console.log(event.target);
+	});
+});
+
